bbe52f2c430078614dad91ba6960c1ed603bccbd
Lucas Menke <lucas_menke@icloud.com>
2023-02-19 21:50:47 +0100

    default structure set up

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import styles from "../constants/style"
+
 export default function AppPage() {
   return (
-    <div className="bg-primary w-full overflow-hidden">AppPage</div>
+    <div className="bg-primary w-full overflow-hidden">
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+            <div className={`${styles.boxWidth}`}></div>
+        </div>
+    </div>
   )
 }
