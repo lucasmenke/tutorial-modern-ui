347769c59396e8221884e0510a906c1682720539
Lucas Menke <lucas_menke@icloud.com>
2023-02-19 22:07:25 +0100

    worked on main layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
-import styles from "../constants/style"
+import styles from "../constants/style";
 
 export default function AppPage() {
   return (
     <div className="bg-primary w-full overflow-hidden">
-        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-            <div className={`${styles.boxWidth}`}></div>
-        </div>
+      {/* Navbar */}
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>Navbar</div>
+      </div>
+      {/* Hero */}
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>Hero</div>
+      </div>
+      {/* Main */}
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>Stats Business Billing</div>
+      </div>
     </div>
-  )
+  );
 }
