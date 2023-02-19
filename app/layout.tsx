e562d1fda715d98f782fd854c014258dcf8b18a5
Lucas Menke <lucas_menke@icloud.com>
2023-02-19 21:40:33 +0100

    fist commit

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import "./globals.css";
+import { Poppins } from "@next/font/google";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+export default function AppPage() {
+  return (
+    <div className="bg-primary w-full overflow-hidden">AppPage</div>
+  )
+}
