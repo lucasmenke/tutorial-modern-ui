391620a6c177151ebcce17b6da3a0a302c448309
Lucas Menke <lucas_menke@icloud.com>
2023-02-20 20:58:23 +0100

    added basic components

---
+++ app/components/index.ts
@@ -0,0 +1,26 @@
+// overlay
+import NavBar from "./overlay/NavBar";
+
+// app-page
+import Billing from "./app-page/Billing";
+import CardDeal from "./app-page/CardDeal";
+import Business from "./app-page/Business";
+import Clients from "./app-page/Clients";
+import CTA from "./app-page/CTA";
+import Stats from "./app-page/Stats";
+import Footer from "./app-page/Footer";
+import Testimonials from "./app-page/Testimonials";
+import Hero from "./app-page/Hero";
+
+export {
+  NavBar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,44 @@
 import styles from "../constants/style";
+import {
+  NavBar,
+  Billing,
+  Business,
+  CardDeal,
+  Clients,
+  CTA,
+  Footer,
+  Stats,
+  Testimonials,
+  Hero,
+} from "./components";
 
 export default function AppPage() {
   return (
     <div className="bg-primary w-full overflow-hidden">
       {/* Navbar */}
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>Navbar</div>
+        <div className={`${styles.boxWidth}`}>
+          <NavBar />
+        </div>
       </div>
       {/* Hero */}
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
       </div>
       {/* Main */}
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Stats Business Billing</div>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
       </div>
     </div>
   );
