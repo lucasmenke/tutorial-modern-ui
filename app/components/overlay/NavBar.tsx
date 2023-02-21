db2f9335278bb19d1ba03c7753f5039f8e4eeee0
Lucas Menke <lucas_menke@icloud.com>
2023-02-21 21:06:27 +0100

    finished navbar

--- app/components/overlay/NavBar.tsx
+++ app/components/overlay/NavBar.tsx
@@ -13,7 +13,7 @@ export default function NavBar() {
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <Image src={logo} alt="HooBank" width={124} height={32} />
       {/* Desktop devices */}
-      <ul className="list-nobe sm:flex hidden justify-end items-center flex-1">
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
@@ -35,8 +35,23 @@ export default function NavBar() {
           className="object-contain"
           onClick={() => setToggle((prev) => !prev)}
         />
-        <div className={`${toggle ? "flex" : "hidden"} p-6 absolute bg-black-gradient top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
-
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 absolute bg-black-gradient top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-base text-white ${
+                  index === navLinks.length - 1 ? "mb-0" : "mb-4"
+                }`}
+              >
+                <Link href={nav.id}>{nav.title}</Link>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </nav>
