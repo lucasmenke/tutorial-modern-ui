d9369e1ce50ecae1e0e74108cd5a6480c30d0f17
Lucas Menke <lucas_menke@icloud.com>
2023-02-21 21:33:13 +0100

    started hero section

--- app/components/app-page/Hero.tsx
+++ app/components/app-page/Hero.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import Image from "next/image";
+import styles from "../../../styles/style";
+import { discount, robot } from "../../../public/assets";
 
 export default function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <section
+      id="home"
+      className={`flex flex-col md:flex-row ${styles.paddingY}`}
+    >
+      <div
+        className={`flex-1 flex-col ${styles.flexStart} xl:px-0 sm:px-16 px-6`}
+      >
+        {/* Discount text */}
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+          <Image src={discount} alt="Discount" height={32} width={32} />
+          <p className={`${styles.paragraph} ml-2`}>
+            <span className="text-white">20%</span> Discount For
+            <span className="text-white"> 1 Month</span> Account
+          </p>
+        </div>
+        {/* Heading */}
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
+            The Next <br className="sm:block hidden" />
+            <span className="text-gradient">Generation</span>
+            <br className="sm:block hidden" /> Payment Methode.
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
 }
