6262e26db523edf6d2c7921b5ce426fec088e52b
Lucas Menke <lucas_menke@icloud.com>
2023-02-22 21:57:17 +0100

    finished hero section

--- app/components/app-page/GetStarted.tsx
+++ app/components/app-page/GetStarted.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import Image from "next/image";
+import styles from "../../../styles/style";
+import { arrowUp } from "../../../public/assets";
 
 export default function GetStarted() {
   return (
-    <div>GetStarted</div>
-  )
+    <div
+      className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
+    >
+      <div
+        className={`${styles.flexCenter} flex-col bg-primary w-full h-full rounded-full`}
+      >
+        <div className={`${styles.flexStart} flex-row+`}>
+          <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
+            <span className="text-gradient">Get</span>
+          </p>
+            <Image src={arrowUp} alt="Arrow" height={23} width={23} className="object-contain" />
+        </div>
+          <p className="font-poppins font-medium text-[18px] leading-[23px]">
+            <span className="text-gradient">Started</span>
+          </p>
+      </div>
+    </div>
+  );
 }
+ 
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Poppins } from "@next/font/google";
 
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["400", "600"],
+  weight: ["400", "500", "600"],
   variable: "--font-poppins",
   display: "swap",
 });
