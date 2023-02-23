108854a6256eb5a7ed6db26d658539f67421d3f5
Lucas Menke <lucas_menke@icloud.com>
2023-02-23 12:27:41 +0100

    added card deal section

--- app/components/app-page/CardDeal.tsx
+++ app/components/app-page/CardDeal.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import Image from "next/image";
+import { card } from "../../../public/assets";
+import styles, { layout } from "../../../styles/style";
+import Button from "./Button";
 
 export default function CardDeal() {
   return (
-    <div>CardDeal</div>
-  )
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Find a better card deal <br className="hidden sm:block" /> in few easy
+          steps.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
+          aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
+        </p>
+
+        <Button styles="mt-10" />
+      </div>
+
+      <div className={layout.sectionImg}>
+        <Image src={card} alt="Card" className="w-full h-full" />
+      </div>
+    </section>
+  );
 }
+  
