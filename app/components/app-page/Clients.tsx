b636ec5954466eea9ad4e81c3af7325cc8e9aeae
Lucas Menke <lucas_menke@icloud.com>
2023-02-23 12:39:42 +0100

    added client section

--- app/components/app-page/Clients.tsx
+++ app/components/app-page/Clients.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import Image from "next/image";
+import { clients } from "../../../constants/constants";
+import styles from "../../../styles/style";
 
 export default function Clients() {
   return (
-    <div>Clients</div>
-  )
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) => (
+          <div
+            key={client.id}
+            className={`flex-1 ${styles.flexCenter} min-w-[120px] sm:min-w-[192px] m-5`}
+          >
+            <Image
+              src={client.logo}
+              alt="Client"
+              className="w-[100px] sm:w-[192px]  object-contain"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
